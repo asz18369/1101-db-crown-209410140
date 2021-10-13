427b6b12c75e2ba6306c36ae99386ee12c2389cb
aa870111 <asz18369@gmail.com>
2021-10-13 21:32:23 +0800

    database

--- utils/database.js
+++ utils/database.js
@@ -2,15 +2,23 @@ const { Pool } = require('pg')
 
 const isProduction = process.env.NODE_ENV === "production"
 
-const pool = new Pool({
-  // user: 'postgres',
-  // host: 'localhost',
-  // database: 'crown_40',
-  // password: '0000',
-  // port: 5432,
-  connectionString: isProduction ? process.env.DATABASE_URL : `postgresql://postgres:0000@localhost:5432/crown40`
-})
+let pool;
 
+if(isProduction){
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false},
+  });
+}else{
+ pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'crown_40',
+  password: '0000',
+  port: 5432,
+  // connectionString: isProduction ? process.env.DATABASE_URL : `postgresql://postgres:0000@localhost:5432/crown40`
+})
+}
 // pool.query('SELECT * from category_40', (err, res) => {
 //   console.log(JSON,stringify(res.rows));
 //   pool.end();
